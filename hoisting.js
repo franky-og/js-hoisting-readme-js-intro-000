86678bd9f0f4f5006996c89203ab12fc32da6546
Franky Liu <cuifranky95@gmail.com>
2019-07-21 21:57:38 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -9,3 +9,23 @@
 
 }
 
+function crazy() {
+  // fix the code in here:
+  var thisIsCrazy = function (){
+    console.log("hey!!!")
+    }
+    thisIsCrazy()
+
+}
+
+function sayMyName() {
+  // fix the code in here:
+  var name = "Cricky";
+
+ sayMy()
+  function sayMy() {
+    var name = "Kristin";
+    console.log(name);
+  }
+ 
+} 
